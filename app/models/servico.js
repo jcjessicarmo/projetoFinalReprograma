f0d069b5f450686fc66d1f5376bae8a98c7e04d3
jcjessicarmo <jessicascarmo@terra.com.br>
2019-12-17 09:19:19 -0300

    Atualizando postServico

--- app/models/servico.js
+++ app/models/servico.js
@@ -1,14 +1,14 @@
 /**
- * Arquivo: produto.js
+ * Arquivo: servico.js
  * Author: Jessica Carmo
  * Descrição: arquivo responsável onde trataremos o modelo da classe 'Serviço'
  * Data: 16/12/2019
  * obs.: http://mongoosejs.com/docs/schematypes.html
  */
 
+ //Conexão MongoDB
  const mongoose = require('mongoose');
  const Schema = mongoose.Schema;
-
  /**
  * Serviço:
  * 
--- server.js
+++ server.js
@@ -16,12 +16,22 @@ const mongoose = require("mongoose");
 
 const Servico = require("./app/models/servico");
 
-//URI
-const MONGO_URL = ("mongodb+srv://Loja:teste231@cluster0-zvfy5.gcp.mongodb.net/test?retryWrites=true&w=majority", 
-{useMongoClient: true});
-
-//Maneira Local: MongoDB
-//mongoose.connect("mongodb://localhost:27017/Loja"), {useMongoClient: true};
+//URI MongoDB Atlas
+//Conexão MongoDB
+const MONGO_URL = ("mongodb+srv://Loja:teste231@cluster0-zvfy5.gcp.mongodb.net/test?retryWrites=true&w=majority");
+function connect () {
+    mongoose.connect(MONGO_URL,
+      { useNewUrlParser: true, useMongoClient: true },
+      function (error) {
+        if(error) {
+          console.error("Ocorreu um erro: ", error)
+        } else {
+          console.log("Conectado no mongoDB.")
+        }
+      }
+    );
+  }
+module.exports = { connect }
 
 // Configuração da variável app para usar o 'bodyParser()'
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -34,23 +44,22 @@ const port = process.env.port || 3000;
 //ROTAS DA API
 // ROUTES - Colocar depois na pasta routers
 //Criando uma instância das rotas via Express:
-var router = express.Router();
+const router = express.Router();
 
-router.use(function (req, res, next){
+router.use((req, res, next) => {
     console.log("Algo está acontecendo aqui...");
     next();
 });
 
 //Rota de exemplo
-router.get("/", function(req, res){
+router.get("/", (req, res) => {
     res.json ({ message: "Bem vinda(o)! a nossa Loja Felisss"})
 });
-
 //==========================================================================
 //API'S
 
 //Rotas que terminarem com '/servicos' (servir: GET ALL & POST)
-router.route("/servicos")
+router.post("/servicos")
 
     /* 1) Método: Criar Serviço (acessar em: POST http://localhost:3000/api/servicos)  */
     const postServico = ((req, res) => {
